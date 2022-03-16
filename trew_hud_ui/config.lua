c2cbd08fe108f45f0f836d6492e21b70857ffc6f
HydraDev <55934542+HydraDevFR@users.noreply.github.com>
2022-03-16 13:15:17 +0100

    Ajout

---
+++ trew_hud_ui/config.lua
@@ -0,0 +1,68 @@
+Config = {}
+
+Config.Locale = 'br'
+
+Config.serverLogo = 'https://i.goopics.net/bqJlK.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= ',',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= true,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= true,
+
+	showHealth			= false,
+	showArmor	 		= false,
+	showStamina	 		= false,
+	showHunger 			= false,
+	showThirst	 		= false,
+
+	showMinimap			= true,
+
+	showWeapons			= true,	
+}
---
+++ trew_hud_ui/server/server.lua
@@ -0,0 +1,44 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+TriggerEvent('es:addCommand', 'toggleui', function()
+end, { help = _U('toggleui') })
+
+RegisterServerEvent('trew_hud_ui:getServerInfo')
+AddEventHandler('trew_hud_ui:getServerInfo', function()
+
+	local source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local job
+	local job2
+
+	if xPlayer ~= nil then
+		if xPlayer.job.label == xPlayer.job.grade_label then
+			job = xPlayer.job.grade_label
+		else
+			job = xPlayer.job.label .. ': ' .. xPlayer.job.grade_label
+		end
+		
+		if xPlayer.job2.label == xPlayer.job2.grade_label then
+			job2 = xPlayer.job2.grade_label
+		else
+			job2 = xPlayer.job2.label .. ': ' .. xPlayer.job2.grade_label
+		end
+
+		local info = {
+			job = job,
+			job2 = job2,
+			money = xPlayer.getMoney(),
+			bankMoney = xPlayer.getAccount('bank').money,
+			blackMoney = xPlayer.getAccount('black_money').money
+		}
+
+		TriggerClientEvent('trew_hud_ui:setInfo', source, info)
+	end
+end)
+
+RegisterServerEvent('trew_hud_ui:syncCarLights')
+AddEventHandler('trew_hud_ui:syncCarLights', function(status)
+	TriggerClientEvent('trew_hud_ui:syncCarLights', -1, source, status)
+end)
